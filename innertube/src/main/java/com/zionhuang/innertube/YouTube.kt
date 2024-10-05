a0d932ee7e17053b1c345d201e8d51411c881bc9
gechoto <124326167+gechoto@users.noreply.github.com>
2024-10-05 21:19:41 +0200

    innertube: add support for premium exclusive albums

--- innertube/src/main/java/com/zionhuang/innertube/YouTube.kt
+++ innertube/src/main/java/com/zionhuang/innertube/YouTube.kt
@@ -185,7 +185,7 @@ object YouTube {
     }
 
     suspend fun albumSongs(playlistId: String): Result<List<SongItem>> = runCatching {
-        val response = innerTube.browse(WEB_REMIX, "VL$playlistId").body<BrowseResponse>()
+        val response = innerTube.browse(WEB_REMIX, "VL$playlistId", setLogin = true).body<BrowseResponse>()
 
         val contents =
             response.contents?.singleColumnBrowseResultsRenderer?.tabs?.firstOrNull()
@@ -201,7 +201,7 @@ object YouTube {
     }
 
     suspend fun artist(browseId: String): Result<ArtistPage> = runCatching {
-        val response = innerTube.browse(WEB_REMIX, browseId).body<BrowseResponse>()
+        val response = innerTube.browse(WEB_REMIX, browseId, setLogin = true).body<BrowseResponse>()
 
         ArtistPage(
             artist = ArtistItem(
@@ -226,7 +226,7 @@ object YouTube {
     }
 
     suspend fun artistItems(endpoint: BrowseEndpoint): Result<ArtistItemsPage> = runCatching {
-        val response = innerTube.browse(WEB_REMIX, endpoint.browseId, endpoint.params).body<BrowseResponse>()
+        val response = innerTube.browse(WEB_REMIX, endpoint.browseId, endpoint.params, setLogin = true).body<BrowseResponse>()
         val gridRenderer = response.contents?.singleColumnBrowseResultsRenderer?.tabs?.firstOrNull()
             ?.tabRenderer?.content?.sectionListRenderer?.contents?.firstOrNull()
             ?.gridRenderer
