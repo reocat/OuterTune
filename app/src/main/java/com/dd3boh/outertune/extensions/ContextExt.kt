bf92bcf066410572ed482d1c2e1351f45f623fba
mattcarter11 <38189440+mattcarter11@users.noreply.github.com>
2024-10-21 20:48:19 +0200

    use dataStorage index get

--- app/src/main/java/com/dd3boh/outertune/extensions/ContextExt.kt
+++ app/src/main/java/com/dd3boh/outertune/extensions/ContextExt.kt
@@ -4,15 +4,14 @@ import android.content.Context
 import com.dd3boh.outertune.constants.InnerTubeCookieKey
 import com.dd3boh.outertune.constants.YtmSyncKey
 import com.dd3boh.outertune.utils.dataStore
+import com.dd3boh.outertune.utils.get
 import com.zionhuang.innertube.utils.parseCookieString
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.runBlocking
 
 fun Context.isSyncEnabled(): Boolean {
     return runBlocking {
-        val ytmSync = dataStore.data.map { it[YtmSyncKey] ?: true }.first()
-        val cookie = dataStore.data.map { it[InnerTubeCookieKey] ?: "" }.first()
+        val ytmSync = dataStore[YtmSyncKey] ?: true
+        val cookie = dataStore[InnerTubeCookieKey] ?: ""
         ytmSync && "SAPISID" in parseCookieString(cookie)
     }
 }
