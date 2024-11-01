3c19311c4852fcc5e5a6a47e3dea4a270fabe979
reocat <ng.ct_ml@tuta.io>
2024-11-01 14:11:16 +0300

    Bump Java target

--- ffMetadataEx/build.gradle.kts
+++ ffMetadataEx/build.gradle.kts
@@ -39,11 +39,11 @@ android {
     ndkVersion = "27.0.11718014"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
