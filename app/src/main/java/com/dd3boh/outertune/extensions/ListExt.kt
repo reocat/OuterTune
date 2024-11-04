8c32cb0146a7c5e12b5fe0b6046402795a95e4b0
reocat <ng.ct_ml@tuta.io>
2024-11-04 07:10:17 +0000

    test

--- app/src/main/java/com/dd3boh/outertune/extensions/ListExt.kt
+++ app/src/main/java/com/dd3boh/outertune/extensions/ListExt.kt
@@ -3,6 +3,19 @@ package com.dd3boh.outertune.extensions
 fun <T> List<T>.reversed(reversed: Boolean) = if (reversed) asReversed() else this
 
 fun <T> MutableList<T>.move(fromIndex: Int, toIndex: Int): MutableList<T> {
-    add(toIndex, removeAt(fromIndex))
+    // First validate the initial indices
+    if (fromIndex !in indices || toIndex !in 0..size) {
+        throw IndexOutOfBoundsException("Invalid indices: fromIndex=$fromIndex, toIndex=$toIndex, size=$size")
+    }
+    
+    val element = removeAt(fromIndex)
+    
+    // After removing the element, we need to adjust the toIndex if it was after fromIndex
+    val adjustedToIndex = when {
+        toIndex > fromIndex -> toIndex - 1
+        else -> toIndex
+    }
+    
+    add(adjustedToIndex, element)
     return this
 }
