droidudc/InnerTune

feat: disable screenshot

app/src/main/java/com/malopieds/innertune/ui/screens/settings/PrivacySettings.kt
@@ -31,6 +31,7 @@ import androidx.navigation.NavController
 import com.malopieds.innertune.LocalDatabase
 import com.malopieds.innertune.LocalPlayerAwareWindowInsets
 import com.malopieds.innertune.R
+import com.malopieds.innertune.constants.DisableScreenshotKey
 import com.malopieds.innertune.constants.EnableKugouKey
 import com.malopieds.innertune.constants.EnableLrcLibKey
 import com.malopieds.innertune.constants.PauseListenHistoryKey
@@ -41,6 +42,7 @@ import com.malopieds.innertune.ui.component.DefaultDialog
 import com.malopieds.innertune.ui.component.IconButton
 import com.malopieds.innertune.ui.component.ListPreference
 import com.malopieds.innertune.ui.component.PreferenceEntry
+import com.malopieds.innertune.ui.component.PreferenceGroupTitle
 import com.malopieds.innertune.ui.component.SwitchPreference
 import com.malopieds.innertune.ui.utils.backToMain
 import com.malopieds.innertune.utils.rememberEnumPreference
@@ -62,11 +64,9 @@ fun PrivacySettings(
             key = PreferredLyricsProviderKey,
             defaultValue = PreferredLyricsProvider.LRCLIB,
         )
+    val (disableScreenshot, onDisableScreenshotChange) = rememberPreference(key = DisableScreenshotKey, defaultValue = false)
 
-    var showClearListenHistoryDialog by remember {
-        mutableStateOf(false)
-    }
-
+    var showClearListenHistoryDialog by remember { mutableStateOf(false) }
     if (showClearListenHistoryDialog) {
         DefaultDialog(
             onDismiss = { showClearListenHistoryDialog = false },
@@ -98,10 +98,7 @@ fun PrivacySettings(
         )
     }
 
-    var showClearSearchHistoryDialog by remember {
-        mutableStateOf(false)
-    }
-
+    var showClearSearchHistoryDialog by remember { mutableStateOf(false) }
     if (showClearSearchHistoryDialog) {
         DefaultDialog(
             onDismiss = { showClearSearchHistoryDialog = false },
@@ -140,23 +137,34 @@ fun PrivacySettings(
     ) {
         Spacer(Modifier.windowInsetsPadding(LocalPlayerAwareWindowInsets.current.only(WindowInsetsSides.Top)))
 
+        PreferenceGroupTitle(
+            title = stringResource(R.string.listen_history),
+        )
+
         SwitchPreference(
             title = { Text(stringResource(R.string.pause_listen_history)) },
             icon = { Icon(painterResource(R.drawable.history), null) },
             checked = pauseListenHistory,
             onCheckedChange = onPauseListenHistoryChange,
         )
+
         PreferenceEntry(
             title = { Text(stringResource(R.string.clear_listen_history)) },
             icon = { Icon(painterResource(R.drawable.delete_history), null) },
             onClick = { showClearListenHistoryDialog = true },
         )
+
+        PreferenceGroupTitle(
+            title = stringResource(R.string.search_history),
+        )
+
         SwitchPreference(
             title = { Text(stringResource(R.string.pause_search_history)) },
             icon = { Icon(painterResource(R.drawable.search_off), null) },
             checked = pauseSearchHistory,
             onCheckedChange = onPauseSearchHistoryChange,
         )
+
         PreferenceEntry(
             title = { Text(stringResource(R.string.clear_search_history)) },
             icon = { Icon(painterResource(R.drawable.clear_all), null) },
@@ -181,6 +189,18 @@ fun PrivacySettings(
             values = listOf(PreferredLyricsProvider.KUGOU, PreferredLyricsProvider.LRCLIB),
             valueText = { it.name.toLowerCase(Locale.current).capitalize(Locale.current) },
             onValueSelected = onPreferredProviderChange,
+        )
+
+        PreferenceGroupTitle(
+            title = stringResource(R.string.misc),
+        )
+
+        SwitchPreference(
+            title = { Text(stringResource(R.string.disable_screenshot)) },
+            description = stringResource(R.string.disable_screenshot_desc),
+            icon = { Icon(painterResource(R.drawable.screenshot), null) },
+            checked = disableScreenshot,
+            onCheckedChange = onDisableScreenshotChange,
         )
     }
 

reocat/OuterTune

app/src/main/java/com/dd3boh/outertune/ui/screens/settings/PrivacySettings.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.automirrored.rounded.ManageSearch
+import androidx.compose.material.icons.filled.Screenshot
 import androidx.compose.material.icons.rounded.ClearAll
 import androidx.compose.material.icons.rounded.History
 import androidx.compose.material.icons.rounded.Person
@@ -33,6 +34,7 @@ import androidx.navigation.NavController
 import com.dd3boh.outertune.LocalDatabase
 import com.dd3boh.outertune.LocalPlayerAwareWindowInsets
 import com.dd3boh.outertune.R
+import com.dd3boh.outertune.constants.DisableScreenshotKey
 import com.dd3boh.outertune.constants.PauseListenHistoryKey
 import com.dd3boh.outertune.constants.PauseSearchHistoryKey
 import com.dd3boh.outertune.constants.UseLoginForBrowse
@@ -55,6 +57,7 @@ fun PrivacySettings(
     val (pauseListenHistory, onPauseListenHistoryChange) = rememberPreference(key = PauseListenHistoryKey, defaultValue = false)
     val (pauseSearchHistory, onPauseSearchHistoryChange) = rememberPreference(key = PauseSearchHistoryKey, defaultValue = false)
     val (useLoginForBrowse, onUseLoginForBrowseChange) = rememberPreference(key = UseLoginForBrowse, defaultValue = false)
+    val (disableScreenshot, onDisableScreenshotChange) = rememberPreference(key = DisableScreenshotKey, defaultValue = false)
 
     var showClearListenHistoryDialog by remember {
         mutableStateOf(false)
@@ -133,6 +136,10 @@ fun PrivacySettings(
     ) {
         Spacer(Modifier.windowInsetsPadding(LocalPlayerAwareWindowInsets.current.only(WindowInsetsSides.Top)))
 
+        PreferenceGroupTitle(
+            title = stringResource(R.string.listen_history)
+        )
+        
         SwitchPreference(
             title = { Text(stringResource(R.string.pause_listen_history)) },
             icon = { Icon(Icons.Rounded.History, null) },
@@ -156,6 +163,17 @@ fun PrivacySettings(
             onClick = { showClearSearchHistoryDialog = true }
         )
 
+        PreferenceGroupTitle(
+            title = stringResource(R.string.misc)
+        )
+        SwitchPreference(
+            title = { Text(stringResource(R.string.disable_screenshot)) },
+            description = stringResource(R.string.disable_screenshot_desc),
+            icon = { Icon(Icons.Default.Screenshot, null) },
+            checked = disableScreenshot,
+            onCheckedChange = onDisableScreenshotChange
+        )
+        
         PreferenceGroupTitle(
             title = stringResource(R.string.account)
         )