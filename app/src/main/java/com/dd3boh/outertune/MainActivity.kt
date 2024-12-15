fb07120ed7ab35c2dfc94029e787caef71dc531e
mikooomich <mikooomichael@gmail.com>
2024-12-15 15:23:35 -0500

    scanners: Support multiple scanners impl

--- app/src/main/java/com/dd3boh/outertune/MainActivity.kt
+++ app/src/main/java/com/dd3boh/outertune/MainActivity.kt
@@ -129,6 +129,8 @@ import com.dd3boh.outertune.constants.PauseSearchHistoryKey
 import com.dd3boh.outertune.constants.PlayerBackgroundStyleKey
 import com.dd3boh.outertune.constants.PureBlackKey
 import com.dd3boh.outertune.constants.ScanPathsKey
+import com.dd3boh.outertune.constants.ScannerImpl
+import com.dd3boh.outertune.constants.ScannerImplKey
 import com.dd3boh.outertune.constants.ScannerMatchCriteria
 import com.dd3boh.outertune.constants.ScannerSensitivityKey
 import com.dd3boh.outertune.constants.ScannerStrictExtKey
@@ -214,7 +216,7 @@ import com.dd3boh.outertune.utils.rememberEnumPreference
 import com.dd3boh.outertune.utils.rememberPreference
 import com.dd3boh.outertune.utils.reportException
 import com.dd3boh.outertune.utils.scanners.LocalMediaScanner
-import com.dd3boh.outertune.utils.scanners.LocalMediaScanner.Companion.unloadAdvancedScanner
+import com.dd3boh.outertune.utils.scanners.LocalMediaScanner.Companion.destroyScanner
 import com.dd3boh.outertune.utils.scanners.ScannerAbortException
 import com.dd3boh.outertune.utils.urlEncode
 import com.valentinilk.shimmer.LocalShimmerTheme
@@ -386,6 +388,10 @@ class MainActivity : ComponentActivity() {
                 key = ScannerSensitivityKey,
                 defaultValue = ScannerMatchCriteria.LEVEL_2
             )
+            val (scannerImpl) = rememberEnumPreference(
+                key = ScannerImplKey,
+                defaultValue = ScannerImpl.TAGLIB
+            )
             val (scanPaths) = rememberPreference(ScanPathsKey, defaultValue = DEFAULT_SCAN_PATH)
             val (excludedScanPaths) = rememberPreference(ExcludedScanPathsKey, defaultValue = "")
             val (strictExtensions) = rememberPreference(ScannerStrictExtKey, defaultValue = false)
@@ -398,7 +404,7 @@ class MainActivity : ComponentActivity() {
                     // Check if the permissions for local media access
                     if (firstSetupPassed && localLibEnable && autoScan
                         && checkSelfPermission(MEDIA_PERMISSION_LEVEL) == PackageManager.PERMISSION_GRANTED) {
-                        val scanner = LocalMediaScanner.getScanner(this@MainActivity)
+                        val scanner = LocalMediaScanner.getScanner(this@MainActivity, scannerImpl)
 
                         // equivalent to (quick scan)
                         try {
@@ -436,7 +442,7 @@ class MainActivity : ComponentActivity() {
                                 Toast.LENGTH_LONG
                             ).show()
                         } finally {
-                            unloadAdvancedScanner()
+                            destroyScanner()
                         }
                         purgeCache() // juuuust to be sure
                         cacheDirectoryTree(null)
