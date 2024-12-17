58f6b8f16faa65cc27f811c5c54f764e295f7bf6
reocat <ng.ct_ml@tuta.io>
2024-12-17 12:52:38 +0300

    Nuke and add material-color-utilities back from InnerTune

--- material-color-utilities/build.gradle.kts
+++ material-color-utilities/build.gradle.kts
@@ -3,10 +3,11 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 dependencies {
     compileOnly(libs.error.prone.core)
+    implementation(libs.annotation)
 }
