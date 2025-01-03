383a6b95a4b62bcefc24bc7ddd57685a4f378dec
mikooomich <mikooomichael@gmail.com>
2025-01-03 13:20:11 -0500

    settings: Update nuke settings

--- app/src/main/java/com/dd3boh/outertune/db/DatabaseDao.kt
+++ app/src/main/java/com/dd3boh/outertune/db/DatabaseDao.kt
@@ -291,6 +291,10 @@ interface DatabaseDao : SongsDao, AlbumsDao, ArtistsDao, PlaylistsDao, QueueDao 
     fun nukeLocalGenre()
 
     @Transaction
+    @Query("DELETE FROM format")
+    fun nukeFormatEntities()
+
+    @Transaction
     fun nukeLocalData() {
         nukeLocalSongs()
         nukeLocalArtists()
