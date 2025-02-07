da9d1d4b6884dbcb3543dd375eebad76bc828aba
mikooomich <mikooomichael@gmail.com>
2025-02-07 16:50:03 -0500

    ui: Correct colour for player error text

--- app/src/main/java/com/dd3boh/outertune/ui/player/PlaybackError.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/player/PlaybackError.kt
@@ -1,6 +1,7 @@
 package com.dd3boh.outertune.ui.player
 
 import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.icons.Icons
@@ -9,6 +10,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
@@ -16,12 +19,33 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.media3.common.PlaybackException
 import com.dd3boh.outertune.R
+import com.dd3boh.outertune.constants.DarkModeKey
+import com.dd3boh.outertune.constants.PlayerBackgroundStyleKey
+import com.dd3boh.outertune.ui.screens.settings.DarkMode
+import com.dd3boh.outertune.ui.screens.settings.PlayerBackgroundStyle
+import com.dd3boh.outertune.utils.rememberEnumPreference
 
 @Composable
 fun PlaybackError(
     error: PlaybackException,
     retry: () -> Unit,
 ) {
+    val playerBackground by rememberEnumPreference(key = PlayerBackgroundStyleKey, defaultValue = PlayerBackgroundStyle.DEFAULT)
+    val darkTheme by rememberEnumPreference(DarkModeKey, defaultValue = DarkMode.AUTO)
+    val isSystemInDarkTheme = isSystemInDarkTheme()
+    val useDarkTheme = remember(darkTheme, isSystemInDarkTheme) {
+        if (darkTheme == DarkMode.AUTO) isSystemInDarkTheme else darkTheme == DarkMode.ON
+    }
+
+    val textColor = when (playerBackground) {
+        PlayerBackgroundStyle.DEFAULT -> MaterialTheme.colorScheme.secondary
+        else ->
+            if (useDarkTheme)
+                MaterialTheme.colorScheme.onSurface
+            else
+                MaterialTheme.colorScheme.onPrimary
+    }
+
     Row(
         horizontalArrangement = Arrangement.spacedBy(12.dp),
         verticalAlignment = Alignment.CenterVertically,
@@ -39,6 +63,7 @@ fun PlaybackError(
 
         Text(
             text = error.cause?.cause?.message ?: stringResource(R.string.error_unknown),
+            color = textColor,
             style = MaterialTheme.typography.bodyMedium
         )
     }
