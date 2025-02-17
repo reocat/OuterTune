c22c3513d6f188462479d8da9a777f6ee225b41b
gechoto <124326167+gechoto@users.noreply.github.com>
2025-02-17 18:24:10 +0000

    app, potoken: replace nanojson with kotlinx serialization (ported from LibreTube PR by @FineFindus)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -183,7 +183,7 @@ dependencies {
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.encoding)
 
-    implementation(libs.nanojson)
+    implementation(libs.kotlinx.serialization.json)
 
     implementation(libs.androidx.webkit)
 
