a2f645f87def2d877a7a77c69ab105bbb1842cf4
mikooomich <mikooomichael@gmail.com>
2025-02-24 19:23:58 -0500

    player: Placeholder image for notification
    * Use app icon as placeholder image
    * Extend placeholder image uses to remote songs too

--- app/src/main/java/com/dd3boh/outertune/utils/CoilBitmapLoader.kt
+++ app/src/main/java/com/dd3boh/outertune/utils/CoilBitmapLoader.kt
@@ -12,11 +12,15 @@ package com.dd3boh.outertune.utils
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Canvas
 import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.net.Uri
+import androidx.core.content.ContextCompat
 import coil.imageLoader
 import coil.request.ErrorResult
 import coil.request.ImageRequest
+import com.dd3boh.outertune.R
 import com.dd3boh.outertune.ui.utils.imageCache
 import com.google.common.util.concurrent.ListenableFuture
 import kotlinx.coroutines.CoroutineScope
@@ -28,7 +32,25 @@ class CoilBitmapLoader(
     private val context: Context,
     private val scope: CoroutineScope,
 ) : androidx.media3.common.util.BitmapLoader {
-    private val placeholderImage = Bitmap.createBitmap(1280, 720, Bitmap.Config.ARGB_8888)
+    private val placeholderImage: Bitmap = drawPlaceholder()
+
+    fun drawPlaceholder(l: Int = 1280, w: Int = 720): Bitmap {
+        val drawable: Drawable? = ContextCompat.getDrawable(context, R.drawable.small_icon)
+        val bitmap = Bitmap.createBitmap(l, w, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+
+        // center without distortion. Expects square drawable
+        if (l > w) {
+            val start = (l - w) / 2
+            drawable?.setBounds(start, 0, start + w, w)
+        } else {
+            val wStart = (w - l) / 2
+            drawable?.setBounds(0, wStart, l, wStart + w)
+        }
+
+        drawable?.draw(canvas)
+        return bitmap
+    }
 
     override fun supportsMimeType(mimeType: String): Boolean {
         return mimeType.startsWith("image/")
@@ -44,7 +66,7 @@ class CoilBitmapLoader(
         scope.future(Dispatchers.IO) {
             // local images
             if (uri.toString().startsWith("/storage/")) {
-                return@future imageCache.getLocalThumbnail(uri.toString(), false)?: placeholderImage
+                return@future imageCache.getLocalThumbnail(uri.toString(), false) ?: placeholderImage
             }
             val result = context.imageLoader.execute(
                 ImageRequest.Builder(context)
@@ -53,12 +75,14 @@ class CoilBitmapLoader(
                     .build()
             )
             if (result is ErrorResult) {
-                throw ExecutionException(result.throwable)
+                reportException(ExecutionException(result.throwable))
+                return@future placeholderImage
             }
             try {
                 (result.drawable as BitmapDrawable).bitmap
             } catch (e: Exception) {
-                throw ExecutionException(e)
+                reportException(ExecutionException(e))
+                return@future placeholderImage
             }
         }
 }
