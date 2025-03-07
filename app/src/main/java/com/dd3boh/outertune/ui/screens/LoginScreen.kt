dcb29c40c278fa2a20e64108fd16e141578eaaba
Ricky Cheung <rcheung844@gmail.com>
2025-03-07 13:02:34 +0800

    ui/login: Disable zoom controls
    This is not necessary and often breaks.
    
    Signed-off-by: Ricky Cheung <rcheung844@gmail.com>

--- app/src/main/java/com/dd3boh/outertune/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/LoginScreen.kt
@@ -85,6 +85,7 @@ fun LoginScreen(
                     javaScriptEnabled = true
                     setSupportZoom(true)
                     builtInZoomControls = true
+                    displayZoomControls = false
                 }
                 addJavascriptInterface(object {
                     @JavascriptInterface
