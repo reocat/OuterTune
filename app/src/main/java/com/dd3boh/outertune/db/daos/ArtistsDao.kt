661722b1b0f5976bbbce4b1f22b18f1111134a83
Davide Garberi <dade.garberi@gmail.com>
2025-03-08 23:46:47 +0100

    app: db: ArtistsDao: Show artists with zero songs as well

--- app/src/main/java/com/dd3boh/outertune/db/daos/ArtistsDao.kt
+++ app/src/main/java/com/dd3boh/outertune/db/daos/ArtistsDao.kt
@@ -145,7 +145,7 @@ interface ArtistsDao {
                 LEFT JOIN song ON sam.songId = song.id
             WHERE $where
             GROUP BY artist.id
-            HAVING songCount > 0 $having
+            HAVING songCount >= 0 $having
             ORDER BY $orderBy
         """)
 
