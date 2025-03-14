cc232ed709bab3819be133b7baab91edc39e869a
mikooomich <mikooomichael@gmail.com>
2025-03-14 16:46:12 -0400

    ui: PlayerMenu: Integrate sleep timer manual input

--- app/src/main/java/com/dd3boh/outertune/ui/menu/PlayerMenu.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/menu/PlayerMenu.kt
@@ -19,6 +19,8 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.PlaylistAdd
 import androidx.compose.material.icons.automirrored.rounded.QueueMusic
@@ -28,6 +30,7 @@ import androidx.compose.material.icons.rounded.Equalizer
 import androidx.compose.material.icons.rounded.Info
 import androidx.compose.material.icons.rounded.LibraryAdd
 import androidx.compose.material.icons.rounded.LibraryAddCheck
+import androidx.compose.material.icons.rounded.MoreTime
 import androidx.compose.material.icons.rounded.Radio
 import androidx.compose.material.icons.rounded.RemoveCircleOutline
 import androidx.compose.material.icons.rounded.Share
@@ -39,9 +42,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -56,6 +61,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.platform.LocalContext
@@ -63,6 +70,7 @@ import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -91,13 +99,11 @@ import com.dd3boh.outertune.ui.component.GridMenu
 import com.dd3boh.outertune.ui.component.GridMenuItem
 import com.dd3boh.outertune.ui.component.ListDialog
 import com.dd3boh.outertune.ui.component.SleepTimerGridMenu
-import com.dd3boh.outertune.ui.component.TextFieldDialog
 import com.zionhuang.innertube.YouTube
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
-import java.lang.Float.parseFloat
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.util.Calendar
@@ -274,6 +280,21 @@ fun PlayerMenu(
                 }
             },
             text = {
+                val focusRequester = remember {
+                    FocusRequester()
+                }
+
+                var showDialog by remember {
+                    mutableStateOf(false)
+                }
+
+                LaunchedEffect(showDialog) {
+                    if (showDialog) {
+                        delay(300)
+                        focusRequester.requestFocus()
+                    }
+                }
+
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     val pluralString = pluralStringResource(
                         R.plurals.minute,
@@ -300,24 +321,6 @@ fun PlayerMenu(
                             ).format(Date(endTime))
                         }
 
-                    var showDialog by remember {
-                        mutableStateOf(false)
-                    }
-
-                    if (showDialog) {
-                        TextFieldDialog(
-                            initialTextFieldValue = TextFieldValue(
-                                text = sleepTimerValue.roundToInt().toString(),
-                                selection = TextRange(0, sleepTimerValue.roundToInt().toString().length),
-                            ),
-                            placeholder = { pluralString },
-                            singleLine = true,
-                            keyboardType = KeyboardType.Number,
-                            isInputValid = { it.toFloatOrNull() != null },
-                            onDone = { sleepTimerValue = it.toFloatOrNull() ?: sleepTimerValue },
-                            onDismiss = { showDialog = false }
-                        )
-                    }
                     Text(
                         text = "$pluralString\n$endTimeString",
                         style = MaterialTheme.typography.bodyLarge,
@@ -328,6 +331,42 @@ fun PlayerMenu(
                                 showDialog = true
                             }
                     )
+
+                    // manual input
+                    if (showDialog) {
+                        val initialText = TextFieldValue(
+                            text = sleepTimerValue.roundToInt().toString(),
+                            selection = TextRange(0, sleepTimerValue.roundToInt().toString().length),
+                        )
+
+                        val (textFieldValue, onTextFieldValueChange) = remember {
+                            mutableStateOf(initialText)
+                        }
+
+                        TextField(
+                            value = textFieldValue,
+                            onValueChange = onTextFieldValueChange,
+                            placeholder = { pluralString },
+                            singleLine = true,
+                            leadingIcon = { Icon(Icons.Rounded.MoreTime, null) },
+                            colors = OutlinedTextFieldDefaults.colors(),
+                            keyboardOptions = KeyboardOptions(
+                                imeAction = ImeAction.Done,
+                                keyboardType = KeyboardType.Number
+                            ),
+                            keyboardActions = KeyboardActions(
+                                onDone = {
+                                    val text = textFieldValue.text.toFloatOrNull()
+                                    if (text != null) {
+                                        sleepTimerValue = textFieldValue.text.toFloatOrNull() ?: sleepTimerValue
+                                    }
+                                }
+                            ),
+                            modifier = Modifier
+                                .weight(weight = 1f, fill = false)
+                                .focusRequester(focusRequester)
+                        )
+                    }
 
                     Slider(
                         value = sleepTimerValue,
