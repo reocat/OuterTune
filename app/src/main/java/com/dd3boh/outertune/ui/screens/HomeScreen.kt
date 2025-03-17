c5e53389cc86779f17ad28a1b428bf62a396d7e1
mikooomich <mikooomichael@gmail.com>
2025-03-17 16:26:19 -0400

    multiqueue: Correct forgotten favourites queue index

--- app/src/main/java/com/dd3boh/outertune/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/HomeScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
@@ -523,10 +524,10 @@ fun HomeScreen(
                             .height(ListItemHeight * rows)
                             .animateItem()
                     ) {
-                        items(
+                        itemsIndexed(
                             items = forgottenFavorites,
-                            key = { it.id }
-                        ) { originalSong ->
+                            key = { _, song -> song.id }
+                        ) { index, originalSong ->
                             val song by database.song(originalSong.id).collectAsState(initial = originalSong)
 
                             SongListItem(
@@ -535,7 +536,8 @@ fun HomeScreen(
                                     playerConnection.playQueue(
                                         ListQueue(
                                             title = queueTitle,
-                                            items = forgottenFavorites.map { it.toMediaMetadata() }
+                                            items = forgottenFavorites.map { it.toMediaMetadata() },
+                                            startIndex = index
                                         )
                                     )
                                 },
