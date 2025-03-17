44fc60ca26b1147ad90a7f7aaaf597b968319880
Davide Garberi <dade.garberi@gmail.com>
2025-03-17 02:03:46 +0100

    ui: Dialog: Invert cancel and Ok buttons

--- app/src/main/java/com/dd3boh/outertune/ui/component/Dialog.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/component/Dialog.kt
@@ -313,7 +313,7 @@ fun ActionPromptDialog(
                 horizontalArrangement = Arrangement.End,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                if (onReset != null)
+                if (onReset != null) {
                     Row(modifier = Modifier.weight(1f)) {
                         TextButton(
                             onClick = { onReset() },
@@ -321,19 +321,21 @@ fun ActionPromptDialog(
                             Text(stringResource(R.string.reset))
                         }
                     }
+                }
+
+                if (onCancel != null) {
+                    TextButton(
+                        onClick = { onCancel() }
+                    ) {
+                        Text(stringResource(android.R.string.cancel))
+                    }
+                }
 
                 TextButton(
                     onClick = { onConfirm() }
                 ) {
                     Text(stringResource(android.R.string.ok))
                 }
-
-                if (onCancel != null)
-                    TextButton(
-                        onClick = { onCancel() }
-                    ) {
-                        Text(stringResource(android.R.string.cancel))
-                    }
             }
         }
     }
