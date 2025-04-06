4298ede3cd36f119c1c2d51748de3adbafaf6e3c
mikooomich <mikooomichael@gmail.com>
2025-04-06 14:21:50 -0400

    settings: Create lyrics fragments

---
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/settings/fragments/LyricFormatFrag.kt
@@ -0,0 +1,84 @@
+package com.dd3boh.outertune.ui.screens.settings.fragments
+
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Lyrics
+import androidx.compose.material.icons.rounded.TextFields
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
+import com.dd3boh.outertune.R
+import com.dd3boh.outertune.constants.LyricFontSizeKey
+import com.dd3boh.outertune.constants.LyricsTextPositionKey
+import com.dd3boh.outertune.ui.component.CounterDialog
+import com.dd3boh.outertune.ui.component.EnumListPreference
+import com.dd3boh.outertune.ui.component.PreferenceEntry
+import com.dd3boh.outertune.ui.screens.settings.LyricsPosition
+import com.dd3boh.outertune.utils.rememberEnumPreference
+import com.dd3boh.outertune.utils.rememberPreference
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ColumnScope.LyricFormatFrag() {
+    val (lyricsPosition, onLyricsPositionChange) = rememberEnumPreference(
+        LyricsTextPositionKey,
+        defaultValue = LyricsPosition.CENTER
+    )
+
+    val (lyricFontSize, onLyricFontSizeChange) = rememberPreference(LyricFontSizeKey, defaultValue = 20)
+
+    var showFontSizeDialog by remember {
+        mutableStateOf(false)
+    }
+
+    EnumListPreference(
+        title = { Text(stringResource(R.string.lyrics_text_position)) },
+        icon = { Icon(Icons.Rounded.Lyrics, null) },
+        selectedValue = lyricsPosition,
+        onValueSelected = onLyricsPositionChange,
+        valueText = {
+            when (it) {
+                LyricsPosition.LEFT -> stringResource(R.string.left)
+                LyricsPosition.CENTER -> stringResource(R.string.center)
+                LyricsPosition.RIGHT -> stringResource(R.string.right)
+            }
+        }
+    )
+    PreferenceEntry(
+        title = { Text(stringResource(R.string.lyrics_font_Size)) },
+        description = "$lyricFontSize sp",
+        icon = { Icon(Icons.Rounded.TextFields, null) },
+        onClick = { showFontSizeDialog = true }
+    )
+
+
+    /**
+     * ---------------------------
+     * Dialogs
+     * ---------------------------
+     */
+
+
+    if (showFontSizeDialog) {
+        CounterDialog(
+            title = stringResource(R.string.lyrics_font_Size),
+            initialValue = lyricFontSize,
+            upperBound = 32,
+            lowerBound = 8,
+            unitDisplay = " pt",
+            onDismiss = { showFontSizeDialog = false },
+            onConfirm = {
+                onLyricFontSizeChange(it)
+                showFontSizeDialog = false
+            },
+            onReset = { onLyricFontSizeChange(20) },
+            onCancel = { showFontSizeDialog = false }
+        )
+    }
+}
