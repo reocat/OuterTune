7303e4116610842f2f7937376faa193d19231bc3
Michael Zh <mikooomichael@gmail.com>
2025-04-14 23:27:10 -0400

    sync: Apply rate limits for various library screens
    * All automatic syncs do not bypass the sync rate limit
    * Add pull to refresh to trigger a manual sync
    * Also move auto sync check to sync utils

--- app/src/main/java/com/dd3boh/outertune/ui/screens/library/LibraryArtistsScreen.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/library/LibraryArtistsScreen.kt
@@ -18,10 +18,15 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.List
 import androidx.compose.material.icons.rounded.GridView
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.pulltorefresh.PullToRefreshDefaults.Indicator
+import androidx.compose.material3.pulltorefresh.pullToRefresh
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -53,6 +58,7 @@ import com.dd3boh.outertune.constants.GridThumbnailHeight
 import com.dd3boh.outertune.constants.LibraryViewType
 import com.dd3boh.outertune.constants.LibraryViewTypeKey
 import com.dd3boh.outertune.constants.SmallGridThumbnailHeight
+import com.dd3boh.outertune.constants.SongFilter
 import com.dd3boh.outertune.extensions.isSyncEnabled
 import com.dd3boh.outertune.ui.component.ChipsRow
 import com.dd3boh.outertune.ui.component.EmptyPlaceholder
@@ -63,8 +69,9 @@ import com.dd3boh.outertune.ui.component.SortHeader
 import com.dd3boh.outertune.utils.rememberEnumPreference
 import com.dd3boh.outertune.utils.rememberPreference
 import com.dd3boh.outertune.viewmodels.LibraryArtistsViewModel
+import kotlinx.coroutines.launch
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun LibraryArtistsScreen(
     navController: NavController,
@@ -88,13 +95,14 @@ fun LibraryArtistsScreen(
 
     val artists by viewModel.allArtists.collectAsState()
     val isSyncingRemoteArtists by viewModel.isSyncingRemoteArtists.collectAsState()
+    val pullRefreshState = rememberPullToRefreshState()
 
     val lazyListState = rememberLazyListState()
     val lazyGridState = rememberLazyGridState()
     val backStackEntry by navController.currentBackStackEntryAsState()
     val scrollToTop = backStackEntry?.savedStateHandle?.getStateFlow("scrollToTop", false)?.collectAsState()
 
-    LaunchedEffect(Unit) { if (context.isSyncEnabled()) viewModel.syncArtists() }
+    LaunchedEffect(Unit) { viewModel.syncArtists() }
 
     LaunchedEffect(scrollToTop?.value) {
         if (scrollToTop?.value == true) {
@@ -117,10 +125,8 @@ fun LibraryArtistsScreen(
                 currentValue = filter,
                 onValueUpdate = {
                     filter = it
-                    if (context.isSyncEnabled()) {
-                        if ((it == ArtistFilter.LIBRARY || it == ArtistFilter.LIKED)
-                            && !isSyncingRemoteArtists) viewModel.syncArtists()
-                    }
+                    if ((it == ArtistFilter.LIBRARY || it == ArtistFilter.LIKED)
+                        && !isSyncingRemoteArtists) viewModel.syncArtists()
                 },
                 modifier = Modifier.weight(1f),
                 isLoading = {
@@ -180,7 +186,15 @@ fun LibraryArtistsScreen(
     }
 
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
+            .fillMaxSize()
+            .pullToRefresh(
+                state = pullRefreshState,
+                isRefreshing = isSyncingRemoteArtists,
+                onRefresh = {
+                    viewModel.syncArtists(true)
+                }
+            ),
     ) {
         when (viewType) {
             LibraryViewType.LIST ->
@@ -282,5 +296,13 @@ fun LibraryArtistsScreen(
                     }
                 }
         }
+
+        Indicator(
+            isRefreshing = isSyncingRemoteArtists,
+            state = pullRefreshState,
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(LocalPlayerAwareWindowInsets.current.asPaddingValues()),
+        )
     }
 }
