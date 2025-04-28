27d854e03894d040a1d41d7b13efd833b88e58e3
Michael Zh <mikooomichael@gmail.com>
2025-04-28 12:25:38 -0400

    app: Remove playerFormat from playerResponseForPlayback
    * Unused for a while now

--- app/src/main/java/com/dd3boh/outertune/playback/DownloadUtil.kt
+++ app/src/main/java/com/dd3boh/outertune/playback/DownloadUtil.kt
@@ -36,7 +36,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -102,11 +101,9 @@ class DownloadUtil @Inject constructor(
             return@Factory dataSpec.withUri(it.first.toUri())
         }
 
-        val playedFormat = runBlocking(Dispatchers.IO) { database.format(mediaId).first() }
         val playbackData = runBlocking(Dispatchers.IO) {
             YTPlayerUtils.playerResponseForPlayback(
                 mediaId,
-                playedFormat = playedFormat,
                 audioQuality = audioQuality,
                 connectivityManager = connectivityManager,
             )
