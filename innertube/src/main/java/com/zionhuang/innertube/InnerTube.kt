0a72182c654b683af1dc29fddcdc9325cf00483e
Michael Zh <mikooomichael@gmail.com>
2025-05-02 17:25:59 -0400

    app: Remove debugging println

--- innertube/src/main/java/com/zionhuang/innertube/InnerTube.kt
+++ innertube/src/main/java/com/zionhuang/innertube/InnerTube.kt
@@ -511,7 +511,6 @@ class InnerTube {
         client: YouTubeClient,
         playlistId: String,
     ) = httpClient.post("playlist/delete") {
-        println("deleting $playlistId")
         ytClient(client, setLogin = true)
         setBody(
             PlaylistDeleteBody(
