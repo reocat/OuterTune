f7faa6e6b70310ac6d2f1b4a3b4488026264c454
Lucas <55422065+lucasmz-dev@users.noreply.github.com>
2025-05-17 23:51:15 +0000

    strings: fix hard-coded 'behaviour' in interface settings (#580)

--- app/src/main/java/com/dd3boh/outertune/ui/screens/settings/InterfaceSettings.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/settings/InterfaceSettings.kt
@@ -287,7 +287,7 @@ fun InterfaceSettings(
         )
 
         PreferenceGroupTitle(
-            title = "behaviour"
+            title = stringResource(R.string.grp_behavior)
         )
         SwitchPreference(
             title = { Text(stringResource(R.string.swipe2Queue)) },
