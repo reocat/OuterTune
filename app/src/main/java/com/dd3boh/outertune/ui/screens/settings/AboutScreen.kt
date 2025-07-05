29b00160651d5c3ab8331b56f266d762d94406db
reocat <ng.ct_ml@tuta.io>
2025-07-05 21:33:45 +0300

    Revert "settings: Add oss libraries and blank contributors screen"
    This reverts commit 9008dda453acb1560ab35d0a405b3e213cf8124d.

--- app/src/main/java/com/dd3boh/outertune/ui/screens/settings/AboutScreen.kt
+++ app/src/main/java/com/dd3boh/outertune/ui/screens/settings/AboutScreen.kt
@@ -58,7 +58,6 @@ import com.dd3boh.outertune.LocalPlayerAwareWindowInsets
 import com.dd3boh.outertune.R
 import com.dd3boh.outertune.constants.TopBarInsets
 import com.dd3boh.outertune.ui.component.IconButton
-import com.dd3boh.outertune.ui.component.PreferenceEntry
 import com.dd3boh.outertune.ui.utils.backToMain
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -200,21 +199,6 @@ fun AboutScreen(
             color = MaterialTheme.colorScheme.secondary
         )
 
-        Spacer(Modifier.height(64.dp))
-/*
-        PreferenceEntry(
-            title = { Text(stringResource(R.string.attribution_title)) },
-            onClick = {
-                navController.navigate("settings/about/attribution")
-            }
-        )
-        PreferenceEntry(
-            title = { Text(stringResource(R.string.oss_licenses_title)) },
-            onClick = {
-                navController.navigate("settings/about/oss_licenses")
-            }
-        )
-*/
         // debug info
         if (BuildConfig.DEBUG) {
             Spacer(Modifier.height(400.dp))
