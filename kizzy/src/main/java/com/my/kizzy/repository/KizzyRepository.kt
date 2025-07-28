8d70facacd4807d5669bf4623b1b17bd88001dbd
Damian Sobczak <sobczakd2017@gmail.com>
2025-07-28 20:34:02 +0200

    feat: Add progress bar to Discord RPC and improvements
    * feat: Add progress bar to discord rpc
    
    * fix: More accurate`startTime` and `endTime`
    
    * feat: Added clickable text and customizable status
    
    * feat: Send presence only when music play
    
    * feat: Added progress bar to preview
    
    * feat: Change Kizzy API url with image cache
    
    * fix: Better action handling without excessive status updates
    
    * remove invalid artist link

--- kizzy/src/main/java/com/my/kizzy/repository/KizzyRepository.kt
+++ kizzy/src/main/java/com/my/kizzy/repository/KizzyRepository.kt
@@ -14,7 +14,6 @@ package com.my.kizzy.repository
 
 import com.my.kizzy.remote.ApiService
 import com.my.kizzy.utils.toImageAsset
-import java.io.File
 
 /**
  * Modified by Zion Huang
@@ -23,8 +22,5 @@ class KizzyRepository {
     private val api = ApiService()
     suspend fun getImage(url: String): String? {
         return api.getImage(url).getOrNull()?.toImageAsset()
-    }
-    suspend fun uploadImage(file: File): String? {
-        return api.uploadImage(file).getOrNull()?.toImageAsset()
     }
 }
