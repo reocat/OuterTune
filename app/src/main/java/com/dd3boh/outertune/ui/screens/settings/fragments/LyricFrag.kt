Bage340/OuterTune-VK

ui: Lyrics fixes (#707)
* Fix lyrics tap going to previous lyric
* Fix lyrics not updating when paused
* Added preference for if lyrics can be tapped

Closes #695

app/src/main/java/com/dd3boh/outertune/ui/screens/settings/fragments/LyricFrag.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.icons.automirrored.rounded.Sort
 import androidx.compose.material.icons.rounded.ContentCut
 import androidx.compose.material.icons.rounded.Lyrics
 import androidx.compose.material.icons.rounded.TextFields
+import androidx.compose.material.icons.rounded.TouchApp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,6 +25,7 @@ import androidx.compose.ui.res.stringResource
 import com.dd3boh.outertune.R
 import com.dd3boh.outertune.constants.EnableKugouKey
 import com.dd3boh.outertune.constants.EnableLrcLibKey
+import com.dd3boh.outertune.constants.LyricClickable
 import com.dd3boh.outertune.constants.LyricFontSizeKey
 import com.dd3boh.outertune.constants.LyricSourcePrefKey
 import com.dd3boh.outertune.constants.LyricTrimKey
@@ -43,6 +45,7 @@ fun ColumnScope.LyricFormatFrag() {
         LyricsTextPositionKey,
         defaultValue = LyricsPosition.CENTER
     )
+    val (syncedLyricsClickable, onSyncedLyricsClickable) = rememberPreference(LyricClickable, defaultValue = true)
 
     val (lyricFontSize, onLyricFontSizeChange) = rememberPreference(LyricFontSizeKey, defaultValue = 20)
 
@@ -68,6 +71,13 @@ fun ColumnScope.LyricFormatFrag() {
         description = "$lyricFontSize sp",
         icon = { Icon(Icons.Rounded.TextFields, null) },
         onClick = { showFontSizeDialog = true }
+    )
+    // clickable lyrics
+    SwitchPreference(
+        title = { Text(stringResource(R.string.lyrics_synced_clickable)) },
+        icon = { Icon(Icons.Rounded.TouchApp, null) },
+        checked = syncedLyricsClickable,
+        onCheckedChange = onSyncedLyricsClickable
     )
 
 

reocat/OuterTune

app/src/main/java/com/dd3boh/outertune/ui/screens/settings/fragments/LyricFrag.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.icons.outlined.ContentCut
 import androidx.compose.material.icons.outlined.Lyrics
 import androidx.compose.material.icons.outlined.SwapVert
 import androidx.compose.material.icons.outlined.TextFields
+import androidx.compose.material.icons.outlined.TouchApp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
@@ -58,6 +59,7 @@ fun ColumnScope.LyricFormatFrag() {
     val (lyricFontSize, onLyricFontSizeChange) = rememberPreference(LyricFontSizeKey, defaultValue = 20)
 
     val (lyricsScroll, onLyricsScrollChange) = rememberPreference(LyricsScrollKey, true)
+    val (lyricsClickable, onLyricsClickableChange) = rememberPreference(LyricClickable, true)
 
     var showFontSizeDialog by remember {
         mutableStateOf(false)
@@ -89,10 +91,16 @@ fun ColumnScope.LyricFormatFrag() {
         title = { Text(stringResource(R.string.lyrics_auto_scroll)) },
         icon = { Icon(Icons.Outlined.SwapVert, "Auto scroll icon") },
         checked = lyricsScroll,
-        onCheckedChange = onLyricsScrollChange,
-        isLast = true
+        onCheckedChange = onLyricsScrollChange
     )
 
+    SwitchPreference(
+        title = { Text(stringResource(R.string.lyrics_synced_clickable)) },
+        icon = { Icon(Icons.Outlined.TouchApp, null) },
+        checked = lyricsClickable,
+        onCheckedChange = onLyricsClickableChange,
+        isLast = true
+    )
 
     /**
      * ---------------------------