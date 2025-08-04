08b8ee8cf7b22cd6cc8af51ecface24db849130b
Daniel <63293432+DanielProg39@users.noreply.github.com>
2025-08-04 05:30:57 +0300

    player: Use PipePipe Extractor to resolve playback issues
    * Replaced LibreTube NewPipe Extractor with PipePipe Extractor

--- innertube/build.gradle.kts
+++ innertube/build.gradle.kts
@@ -14,6 +14,6 @@ dependencies {
     implementation(libs.ktor.serialization.json)
     implementation(libs.ktor.client.encoding)
     implementation(libs.brotli)
-    implementation(libs.newpipe.extractor)
+    implementation(libs.pipepipe.extractor)
     testImplementation(libs.junit)
 }
--- innertube/src/main/java/com/zionhuang/innertube/NewPipe.kt
+++ innertube/src/main/java/com/zionhuang/innertube/NewPipe.kt
@@ -7,6 +7,7 @@ import io.ktor.http.parseQueryString
 import okhttp3.OkHttpClient
 import okhttp3.RequestBody.Companion.toRequestBody
 import org.schabi.newpipe.extractor.NewPipe
+import org.schabi.newpipe.extractor.downloader.CancellableCall
 import org.schabi.newpipe.extractor.downloader.Downloader
 import org.schabi.newpipe.extractor.downloader.Request
 import org.schabi.newpipe.extractor.downloader.Response
@@ -56,9 +57,12 @@ private class NewPipeDownloaderImpl(proxy: Proxy?) : Downloader() {
         val responseBodyToReturn = response.body?.string()
 
         val latestUrl = response.request.url.toString()
-        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, latestUrl)
+        return Response(response.code, response.message, response.headers.toMultimap(), responseBodyToReturn, responseBodyToReturn?.toByteArray(), latestUrl)
     }
 
+    override fun executeAsync(request: Request, callback: AsyncCallback?): CancellableCall {
+        TODO("Placeholder")
+    }
 }
 
 object NewPipeUtils {
@@ -94,5 +98,4 @@ object NewPipeUtils {
                 url
             )
         }
-
 }
