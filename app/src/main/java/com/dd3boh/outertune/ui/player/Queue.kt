Bage340/OuterTune-VK

ui: Fix songs being reorderable in queue even though queue is locked

app/src/main/java/com/dd3boh/outertune/ui/player/Queue.kt
@@ -729,15 +729,13 @@ fun BoxScope.QueueContent(
                                         )
                                     }
                                     if (!lockQueue && !detachedHead) {
-                                        IconButton(
-                                            onClick = { },
-                                            modifier = Modifier.draggableHandle()
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Rounded.DragHandle,
-                                                contentDescription = null
-                                            )
-                                        }
+                                        Icon(
+                                            imageVector = Icons.Rounded.DragHandle,
+                                            contentDescription = null,
+                                            modifier = Modifier
+                                                .padding(end = 16.dp)
+                                                .draggableHandle()
+                                        )
                                     }
                                 }
                             },
@@ -774,7 +772,6 @@ fun BoxScope.QueueContent(
                                         }
                                     }
                                 )
-                                .longPressDraggableHandle()
                         )
                     }
 

reocat/OuterTune

app/src/main/java/com/dd3boh/outertune/ui/player/Queue.kt
@@ -745,15 +745,13 @@ fun BoxScope.QueueContent(
                                         )
                                     }
                                     if (!lockQueue && !detachedHead) {
-                                        IconButton(
-                                            onClick = { },
-                                            modifier = Modifier.draggableHandle()
-                                        ) {
-                                            Icon(
-                                                imageVector = Icons.Outlined.DragHandle,
-                                                contentDescription = null
-                                            )
-                                        }
+                                        Icon(
+                                            imageVector = Icons.Outlined.DragHandle,
+                                            contentDescription = null,
+                                            modifier = Modifier
+                                                .padding(end = 16.dp)
+                                                .draggableHandle()
+                                        )
                                     }
                                 }
                             },
@@ -790,7 +788,6 @@ fun BoxScope.QueueContent(
                                         }
                                     }
                                 )
-                                .longPressDraggableHandle()
                         )
                     }
 